c25b1eadfe43707b7ac00834cf7986a4025f8821
balawahu <627482647@qq.com>
2019-03-21 08:35:03 +0800

    more works

--- Session4/S4-ArrayMesh/js/index.js
+++ Session4/S4-ArrayMesh/js/index.js
@@ -1,5 +1,8 @@
 var renderer, scene, camera;
 var controls;
+var cubes = [];
+var rot = 0;
+
 function init() {
   scene = new THREE.Scene();
 
@@ -7,7 +10,7 @@ function init() {
       H = window.innerHeight;
 
   camera = new THREE.PerspectiveCamera(45, W / H, .1, 1000);
-  camera.position.set(0, 55, 85);
+  camera.position.set(20, 50, 85);
   camera.lookAt(scene.position);
 
   var spotLight = new THREE.SpotLight(0xFFFFFF);
@@ -16,70 +19,109 @@ function init() {
   //spotLight.castShadow = true;
 
   var ambLight = new THREE.AmbientLight(0xFFFFFF);
-  ambLight.position.set(0,1000,0);
-  ambLight.add(spotLight);
-  scene.add(ambLight);
+ambLight.position.set(0,1000,0);
+ambLight.add(spotLight);
+scene.add(ambLight);
+
 
   renderer = new THREE.WebGLRenderer({antialias:true});
   renderer.setClearColor(0x17293a);
   renderer.setSize(W, H);
   //renderer.shadowMapEnabled = true;
 
- controls = new THREE.OrbitControls(camera,renderer.domElement);
 
+controls = new THREE.OrbitControls(camera,renderer.domElement);
   //Create a two dimensional grid of objects, and position them accordingly
-  for (var x = -10; x <=10; x += 5) { // Start from -45 and sequentially add one every 5 pixels
-  for (var y = -10; y < =10; y += 5) {
-    for (var z = -10; z <= 10; z += 5) {
+  for (var x = -10; x <= 10; x += 5) { // Start from -45 and sequentially add one every 5 pixels
+    for (var y = -10; y <= 10; y += 5) {
+        for (var z= -10; z <= 10; z += 5) {
 
-
-    //Concatenation of the x and y values (open Console to see)
-    console.log("X:"+x+",Y:"+y+",Z:"+z);
-
+//Concatenation of the x and y values (open console to see)
+console.log("X:" +x+ ",Y : " +y+ ",Z:" +z);
 
       var boxGeometry = new THREE.BoxGeometry(3, 3, 3);
-  var boxMaterial = new THREE.MeshLambertMaterial({color: 0xFFFFFF});
-//The color of the material is assigned a random color
+      var boxMaterial = new THREE.MeshLambertMaterial({color:  0xFFFFFF});
+      //The color of the material is assigned a random color
 
-   if (  x >=0  && y >=0  && z >=0){
-//x >= 0 this means that squares with values 0 and 5 are true
-//y >= 0 this means that squares with values 0 and 5 are true
-//z >= 0 this means that squares with values 0 and 5 are true
- boxMaterial = new THREE.MeshLambertMaterial({color: 0xF67280});
-} else if( x <=0 &&  y>=0  &&  z>= 0){
-  boxMaterial = new THREE.MeshLambertMaterial({color: 0xFF0000});
-} else if ( x <=0 &&  y>=0  &&  z>= 0){
-    boxMaterial = new THREE.MeshLambertMaterial({color:0xF0F0F0});
-} else if ( x <=0 &&  y<=0  &&  z>= 0){
-  boxMaterial = new THREE.MeshLambertMaterial({color:0xF0F0F0});
-}  else if ( x <=0 &&  y<=0  &&  z>= 0){
-    boxMaterial = new THREE.MeshLambertMaterial({color:0xF0F0F0});
-  }  else if ( x <=0 &&  y>=0  &&  z<= 0){
-      boxMaterial = new THREE.MeshLambertMaterial({color:0xF0F0F0});
-    }  else if ( x <=0 &&  y>=0  &&  z<= 0){
-        boxMaterial = new THREE.MeshLambertMaterial({color:0xF25756});
-      }  else if ( x <=0 &&  y<=0  &&  z<= 0){
-          boxMaterial = new THREE.MeshLambertMaterial({color:0xF60720});
+  if (x >= 0 && y >= 0 && z >= 0){
+         boxMaterial = new THREE.MeshLambertMaterial({color: 0xF67280});
+        }else if ( x <= 0 && y >=0 && z >= 0){
+        boxMaterial = new THREE.MeshLambertMaterial({color: 0x9F79EE});
+      }else if ( x >= 0 && y <=0 && z >= 0){
+        boxMaterial =new THREE.MeshLambertMaterial({color:0xEEC900});
+      }else if ( x <= 0 && y <=0 && z >= 0){
+        boxMaterial =new THREE.MeshLambertMaterial({color:0xF98777});
+      }else if ( x >= 0 && y >=0 && z <= 0){
+        boxMaterial =new THREE.MeshLambertMaterial({color:0x7EC0EE});
+      }else if ( x <= 0 && y >=0 && z <= 0){
+        boxMaterial =new THREE.MeshLambertMaterial({color:0xF72890});
+      }else if ( x >= 0 && y <=0 && z <= 0){
+        boxMaterial =new THREE.MeshLambertMaterial({color:0xF26278});
+      }
+
+/*if (x >= 0 && y >= 0){
+  var boxMaterial = new THREE.MeshLambertMaterial({color:  0xFF00FF});
 }
 
+if (x >= 0 && y <= 0){
+  var boxMaterial = new THREE.MeshLambertMaterial({color:  0xFF0000});
+}
 
-    var mesh = new THREE.Mesh(boxGeometry, boxMaterial);
+if (x <= 0 && y >= 0){
+  var boxMaterial = new THREE.MeshLambertMaterial({color:  0x0000FF});
+}
+
+if (x <= 0 && y <= 0){
+  var boxMaterial = new THREE.MeshLambertMaterial({color:  0x00FF00});
+}
+*/
+
+    //  var boxMaterial = new THREE.MeshLambertMaterial({color: Math.random() * 0xFFFFFF});
+      var mesh = new THREE.Mesh(boxGeometry, boxMaterial);
       //mesh.castShadow = true;
       mesh.position.x = x;
-      mesh.position.z = 0;
-      mesh.position.y = y;
-      mesh.scale.y = 0.5;
+      mesh.position.z = z;
+        mesh.position.y = y;
+    //  mesh.scale.y = 0.5;
       scene.add(mesh);
-  }
+      cubes.push(mesh);
 }
 }
+}
+console.log(cubes);
+document.body.appendChild(renderer.domElement);
+
+//}
+  /*for (var x = -50; x <= 50; x += 5) { // Start from -45 and sequentially add one every 5 pixels
+    //for (var y = -30; y <= 30; y += 5) {
+      var boxGeometry = new THREE.BoxGeometry(3, 6, 3);
+      //The color of the material is assigned a random color
+      var boxMaterial = new THREE.MeshLambertMaterial({color: Math.random() * 0xFFFFFF});
+      var mesh2 = new THREE.Mesh(boxGeometry, boxMaterial);
+      //mesh.castShadow = true;
+      mesh2.position.x = x;
+      mesh2.position.y = 5;
+      mesh2.position.z = 0;
+      mesh2.scale.y = 0.5;
+      scene.add(mesh2);
+  }*/
+
 
   document.body.appendChild(renderer.domElement);
-}
 
+
+}
 function drawFrame(){
-  requestAnimationFrame(drawFrame);
+requestAnimationFrame(drawFrame);
   renderer.render(scene, camera);
+
+rot += 0.01;
+  cubes.forEach(function(c,i) {
+  c.rotation.x = rot;
+  c.rotation.y = rot;
+c.scale.x = rot;
+  });
+    renderer.render(scene, camera);
 }
 
 init();
